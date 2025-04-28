ee721b5a6bac95d9c86bcde9cd9439e710907907
Lucky <sashankaswain6@gmail.com>
2025-04-29 02:23:17 +0530

    FEAT : fetch cityId and address and name

--- src/models/flight.js
+++ src/models/flight.js
@@ -17,11 +17,15 @@ module.exports = (sequelize, DataTypes) => {
 
       this.belongsTo(models.Airport, {
         foreignKey : 'departureAirportId',
+        as : 'departureAirport',
+        targetKey : 'code',
         onDelete : 'CASCADE',
         onUpdate : 'CASCADE'
       });
       this.belongsTo(models.Airport, {
         foreignKey : 'arrivalAirportId',
+        as : 'arrivalAirport',
+        targetKey : 'code',
         onDelete : 'CASCADE',
         onUpdate : 'CASCADE'
       });
--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -2,7 +2,7 @@ const crudRepository = require("./crud-reository");
 const AppError = require("../utils/errors/app-errors");
 const { StatusCodes } = require("http-status-codes");
 
-const { Flight } = require("../models");
+const { Flight , Airport } = require("../models");
 
 class FlightRepository extends crudRepository {
   constructor() {
@@ -10,7 +10,22 @@ class FlightRepository extends crudRepository {
   }
   async getAllFlights(filter, sort) {
     try {
-      const flights = await this.model.findAll({ where: filter, order: sort });
+      const flights = await this.model.findAll({
+        where: filter,
+        order: sort,
+        include: [
+          {
+            model: Airport,
+            as: "departureAirport",
+            attributes: ["id", "name", "cityID", "address"],
+          },
+          {
+            model: Airport,
+            as: "arrivalAirport",
+            attributes: ["id", "name", "cityID", "address"],
+          },
+        ],
+      });
       return flights;
     } catch (error) {
       console.log("Unexpected Error: ", error);
