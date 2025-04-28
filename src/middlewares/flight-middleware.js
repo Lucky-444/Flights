54273ae415c18d3f400aa75159fda5b4829d5309
Lucky <sashankaswain6@gmail.com>
2025-04-29 00:25:38 +0530

    fix : flight-middlewares

--- src/middlewares/flight-middleware.js
+++ src/middlewares/flight-middleware.js
@@ -23,42 +23,60 @@ function validateCreateRequest(req, res, next) {
     return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
   }
 
-  if(!req.body.departureAirportId){
-        ErrorResponse.message = "Something Went Wrong";
-        ErrorResponse.error = new AppError(
-          ["departureId should not be null"],
-          StatusCodes.BAD_REQUEST
-        );
-    
-        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  if (!req.body.departureAirportId) {
+    ErrorResponse.message = "Something Went Wrong";
+    ErrorResponse.error = new AppError(
+      ["departureId should not be null"],
+      StatusCodes.BAD_REQUEST
+    );
+
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
   }
 
-  if(!req.body.arrivalAirportId){
-        ErrorResponse.message = "Something Went Wrong";
-        ErrorResponse.error = new AppError(
-          ["arrivalId should not be null"],
-          StatusCodes.BAD_REQUEST
-        );
-    
-        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  if (!req.body.arrivalAirportId) {
+    ErrorResponse.message = "Something Went Wrong";
+    ErrorResponse.error = new AppError(
+      ["arrivalId should not be null"],
+      StatusCodes.BAD_REQUEST
+    );
+
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
   }
-  if(!req.body.price){
-        ErrorResponse.message = "Something Went Wrong";
-        ErrorResponse.error = new AppError(
-          ["price should not be null"],
-          StatusCodes.BAD_REQUEST
-        );
-    
-        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  if (!req.body.price) {
+    ErrorResponse.message = "Something Went Wrong";
+    ErrorResponse.error = new AppError(
+      ["price should not be null"],
+      StatusCodes.BAD_REQUEST
+    );
+
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
   }
-  if(!req.body.totalSeats){
-        ErrorResponse.message = "Something Went Wrong";
-        ErrorResponse.error = new AppError(
-          ["totalSeats should not be null"],
-          StatusCodes.BAD_REQUEST
-        );
-    
-        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  if (!req.body.totalSeats) {
+    ErrorResponse.message = "Something Went Wrong";
+    ErrorResponse.error = new AppError(
+      ["totalSeats should not be null"],
+      StatusCodes.BAD_REQUEST
+    );
+
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+  if (!req.body.arrivalTime) {
+    ErrorResponse.message = "Something Went Wrong";
+    ErrorResponse.error = new AppError(
+      ["arrivalTime should not be null"],
+      StatusCodes.BAD_REQUEST
+    );
+
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+  if (!req.body.departureTime) {
+    ErrorResponse.message = "Something Went Wrong";
+    ErrorResponse.error = new AppError(
+      ["departureTime should not be null"],
+      StatusCodes.BAD_REQUEST
+    );
+
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
   }
   next();
 }
